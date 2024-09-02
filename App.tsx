7e5884150ef701cbdfc8562c73b0c8b38a2c3b65
HimanshuTakshak2000 <himanhutakshak2000@gmail.com>
2024-09-02 14:00:44 +0530

    What is useEffect Hook  with help of example in react-native vd-24 and vd-25 by Step-By-Step

--- App.tsx
+++ App.tsx
@@ -1,130 +1,101 @@
-/* eslint-disable react-native/no-inline-styles */
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react'
+import { Button, View,Text } from 'react-native'
 
-import {Text, View, TextInput, Button,StyleSheet, ScrollView} from 'react-native';
-import AppStyleSheet from './Style/AppStyleSheet';
+export default function App() {
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState(10);
+  const [countProp1, setCountProp1 ] = useState(100);
+  const [countProp2, setCountProp2 ] = useState(1000);
 
-// Form in react-native :-
+  /* 
+  Note :- 1) We can use unlimited useEffect in single functional component.
+          2) useEffect Hook take two argument i.e first is call back function and second is an array.
+          3) useEffect Hook is called every time when state is updated, props are updated or UI is render for the first time. If we want useEffect is only called for the first time when UI is render then we have to use second argument of useEffect i.e an array either an empty array or array with the state in it as it's element.
+          4) This is how useEffect will be called when props are updated. 
+  */
+  useEffect(() => {
+    console.log('called first time')
+  },[])
 
-function App() {
-  
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [display, setDisplay] = useState(false);
-  const [passwordText, setPasswordText] = useState('Show');
-  const [secureTextEntryStatus, setSecureTextEntryStatus] = useState(true);
-  const [buttonDisplay, setButtonDisplay] = useState(true);
+  useEffect(() => {
+    console.log('-----------------> called when Count is updated')
+  },[count])
 
-  function handleSubmit(){
-    // if(display === false){
-    //   setDisplay(true);
-    //   // setEmail('');
-    //   // setName('');
-    //   // setPassword('');
-    // }
-    // else{
-    //   setDisplay(false);
-    // }
-    
-    setDisplay(true);
-    setButtonDisplay(false);
-  }
+  useEffect(() => {
+    console.log('+++++++++++++ called when Data is updated')
+  },[data])
 
-
-  const handleClear = function(){
-    setDisplay(false);
-    setButtonDisplay(true);
-    setEmail('');
-    setName('');
-    setPassword('');
-  }
-
-  const handleToggle = ()=>{
-    if('Show' === passwordText){
-      setPasswordText('Hide');
-      setSecureTextEntryStatus(false);
-    }
-    else{
-      setPasswordText('Show');
-      setSecureTextEntryStatus(true);
-    }
-  }
   return (
-    <ScrollView>
-      <Text style={{textAlign:'center', fontSize:40,}}>Application Form</Text>
+    <>
+    <Button
+      title={`Increment Count :- ${count}`}
+      onPress={() => {
+        setCount(count + 1)
+      }}
+    />
+    <View style={{marginTop:20}}>
+    <Button
+      title={`Increment Data :- ${data}`}
+      color={'red'}
+      onPress={() => {
+        setData(data + 1)
+      }}
+    />
+    </View>
 
-      <View style={AppStyleSheet.ViewBox1}>
+    <View style={{marginTop:20}}>
+    <Button
+      title={`Increment CountProp1 :- ${countProp1}`}
+      color={'skyblue'}
+      onPress={() => {
+        setCountProp1(countProp1 + 1)
+      }}
+    />
+    </View>
 
-        <View style={AppStyleSheet.ViewBox2}>
-          <Text style={AppStyleSheet.textBox1}>Name</Text>
-          <TextInput style={AppStyleSheet.textInput} placeholder='Enter Name' onChangeText={(text)=> setName(text)} value={name} />
-        </View>
+    <View style={{marginTop:20}}>
+    <Button
+      title={`Increment CountProp2 :- ${countProp2}`}
+      color={'blue'}
+      onPress={() => {
+        setCountProp2(countProp2 + 1)
+      }}
+    />
+    </View>
 
-        <View style={AppStyleSheet.ViewBox2}>
-          <Text style={AppStyleSheet.textBox1}>Email</Text>
-          <TextInput style={AppStyleSheet.textInput} placeholder='Enter Email' onChangeText={(text)=> setEmail(text)} value={email} />
-        </View>
-
-        <View style={AppStyleSheet.ViewBox2}>
-          <Text style={AppStyleSheet.textBox1}>Password</Text>
-
-          <View style={internalStyleSheet.flexBoxView}>
-            <TextInput style={AppStyleSheet.passwordInput} placeholder='Enter Password' onChangeText={(text)=> setPassword(text)} value={password} secureTextEntry={secureTextEntryStatus} />
-
-            <Button title={passwordText} onPress={handleToggle}/>
-            
-          </View>
-          
-        </View>
-
-        <View style={AppStyleSheet.ViewBox2}>
-          <View>
-            {buttonDisplay? <Button title='Submit' onPress={handleSubmit}/> : <Button title='clear' onPress={() => handleClear()}/>}
-            
-          </View>
-
-        </View>
-
-        
-        
-      </View>
-
-      {
-        display?
-          <View 
-          style={internalStyleSheet.submitDisplay}
-          >
-            <Text style={AppStyleSheet.textBox1}>Name :- {name}</Text>
-            <Text style={AppStyleSheet.textBox1}>Email :- {email}</Text>
-            <Text style={AppStyleSheet.textBox1}>Password :- {password}</Text>
-          </View>
-          :
-          null
-      }
-
-    </ScrollView>
-  );
-
-  
+    <PropComponent Prop = {{countProp1, countProp2}} />
+    </>
+  )
 }
 
-const internalStyleSheet = StyleSheet.create({
 
-  flexBoxView:{
+const PropComponent = ({Prop}:any) =>{
 
-    display:"flex", 
-    flexDirection:'row', 
-    justifyContent:"space-between", 
-    alignItems:"center",
-  },
+  useEffect(() => {
+    console.log('========> called when CountProp1 is updated')
+  },[Prop.countProp1])
 
-  submitDisplay:{
-    backgroundColor:"white", 
-    marginTop:10, 
-    padding:10
-  }
-});
+  useEffect(() => {
+    console.log('............ called when CountProp2 is updated')
+  },[Prop.countProp2])
 
+  useEffect(() => {
+    console.log('=================== called Every time in PropComponent component ====================')
+  },)
 
-export default App;
+  useEffect(() => {
+    console.log('///////////////////// called first time in PropComponent component //////////////////////')
+  },[])
+
+  return(
+    <View style={{marginTop:20, alignItems:'center'}}>
+      <Text style={{marginTop:5}}>
+      count Prop 1 :- {Prop.countProp1}
+    </Text>
+
+    <Text style={{marginTop:5}}>
+      count Prop 2 :- {Prop.countProp2}
+    </Text>
+    </View>
+  )
+}
