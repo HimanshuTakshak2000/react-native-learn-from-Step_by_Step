b0c8586ce7931b6f62e16a5c5bd13bec8fc2a348
HimanshuTakshak2000 <himanhutakshak2000@gmail.com>
2024-09-04 13:29:40 +0530

    useLayoutEffect hook with help of example and difference between useLayoutEffect && useEffect hook in react-native vd-unknown by Step-By-Step

--- App.tsx
+++ App.tsx
@@ -1,55 +1,36 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react'
+import React, { useState, useEffect, useLayoutEffect } from 'react'
 import { Button, View,Text, TextInput } from 'react-native'
 import NavBar from './Components/NavBar'
 
 export default function App() {
 
-  /* useCallback Hook
-  Note :- 1) The useCallback hook is a React hook that allows you to cache a function and return a memoized version of it. This can be  
-          used to optimize performance in React Native apps by avoiding unnecessary re-renders. It is similar to the useMemo hook but instead of returning a value, it returns a function.
-          2) The useCallback hook takes two arguments: a function and an array of dependencies. The function is the callback that is memoized and the array of dependencies is used to determine when the memoized version of the callback should be recreated.
-          3) The useCallback hook should be used when you need to memoize a callback function that is used multiple times. This can be used to optimize performance in React Native apps by avoiding unnecessary re-renders.
-          4) In another word, if we apply use callback hook to a function then it will freez the re-rendering of this function and if we pass any element in the array in callback hook then it will only be render that elemnt has update and other case it will be freezed.
-          5) If we pass any function as prop to component then it would be always different from the previous function even if we have not updated the function. So, the component is re-render everytime where we have used function as it's prop of the component.
-          6) When the UI is re-render then function are re-created. This phenomena is called as referential equality in React JS.
-          7) We can stop re-rendring of component if noting is updated i.e props using memo function with the class name.
+  /* useLayoutEffect Hook
+  Note :- 1) useLayoutEffect hook runs before the DOM is printed on the browser.
+          2) When ever we want to run code before the DOM is printed such as height, width and Anyting related to layout.
+          3) useLayoutEffect hook is syncrynous and useEffect hook is asyncronous.
+          4) The most common use case of useLayoutEffect is to get the dimension of the layout. 
+          4) useEffect hook runs after the DOM is printed on the browser.
+          5) useLayoutEffect hook works exactly the same as useEffect(with same syntax).The only difference is when it run.
   */
+  const [toggle, setToggle] = useState(false);
 
-  const [count, setCount] = useState(0);
-  const [adjective, setAdjective] = useState("Good");
-  
-  // function updateAdjective (){
-  //   return "Very Good";
-  // }
+  useEffect(()=>{
+    console.log('\n++++++++++ useEffect hook is called');
+    // setTimeout(()=>{console.log('useEffect set time out ')},3000); // useEffect is asyncrynous
+    console.log("end of useEffect hook ++++++++++\n");
+  },[toggle]);
 
-  const updateAdjective =  useCallback(()=>{
-    return "Very Good";
-  },[]); // if we pass count in the array then <Nav /> component will also be re-render for count update and if we do not write count then will not be re-render for count update.
+  useLayoutEffect(()=>{
+    console.log('\n---------- useLayoutEffect hook is called');
+    // setTimeout(()=>{console.log('useLayoutEffect set time out ')},3000); // useLayoutEffect is syncrynous
+    console.log("end of useLayoutEffect hook ----------\n");
+  },[toggle]);
 
   return (
 
-    <View>
-      <View style={{margin:15}}>
-      {/* <NavBar adjective ={adjective} /> */}
-        <NavBar adjective ={adjective} setAdjective={setAdjective} updateAdjective={updateAdjective} />
-      </View>
-
-      <View style={{margin:15}}>
-        <Text style={{fontSize:20, marginBottom:10}}>Count :- {count}</Text>
-        <Button title='Increase Counter' onPress={()=>setCount(count+1)}/>
-      </View>
-
-      <View style={{margin:15}}>
-        <Text style={{fontSize:20, marginBottom:10}}>Adjective :- {adjective}</Text>
-        <Button title='Increase Counter' onPress={()=> {
-        if(adjective === 'Good'){
-            setAdjective('Bad')
-        }
-        else{
-            setAdjective('Good')
-        }
-      }}/>
-      </View>
+    <View style={{margin:15}}>
+      <Button title={toggle?"Hide":"Show"} onPress={()=> setToggle(!toggle)} />
+        {toggle && <Text style={{marginTop:10, fontSize:20}}>Text is vissible</Text>}
 
     </View>
 
