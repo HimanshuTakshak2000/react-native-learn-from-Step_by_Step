46aa03c79b2d0686a03868ca20569c5cdb5c5f6a
HimanshuTakshak2000 <himanhutakshak2000@gmail.com>
2024-09-06 18:47:54 +0530

    what is reducer in redux  with help of example in react-native vd-68, vd-69 and vd-70 by Step-By-Step

---
+++ Components/redux/rootReducer.js
@@ -0,0 +1,12 @@
+import {combineReducers} from 'redux';
+import reducer from './reducer';
+/*
+    1) if we have more then one reducer in redux then we have to use combineReducers.
+    2) If we have only one reducer in redux then it is good practices to write combineReducers.
+    import userReducer from './userReducer';
+*/
+
+combineReducers({
+    reducer,
+    // userReducer, and so on... if we have more reducer in your App. 
+})
