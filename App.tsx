950f10036d7629882e5b2b857d4e757e6585ca2c
HimanshuTakshak2000 <himanhutakshak2000@gmail.com>
2024-09-12 10:35:46 +0530

    Created API using JSON server and using JSON server api done get request with help of example in react-native vd-49, vd-50 and vd-51 by Step-By-Step

--- App.tsx
+++ App.tsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { FlatList, ScrollView, Text, View } from 'react-native';
 
 interface Get {
-  id: number;
-  title: string;
-  body: string;
+  id: string;
+  name: string;
+  class: string;
+  age:string;
 }
 
 
@@ -12,12 +13,16 @@ export default function App() {
 
   /* API Calling :- 
   Note :- 1) Get Method and Displaying USer List in scroll-view.
+          2) First use this command - ipconfig to find the ip of your device.
+          3) Then use this command - json-server --host 0.0.0.0 db.json to start the server.
+          4) Then enter this - http://192.168.29.59:3000 url into the web where 192.168.29.59 is the ip of your device.
+          2) http://192.168.29.59:3000/users --> now change the url as :- http://10.0.2.2:3000/users . To use into your code.
   */
 
   const [data, setData] = useState<Get[]| undefined>(undefined);
 
-  const url = "https://jsonplaceholder.typicode.com/posts";
-  const getAPIData = async()=>{
+  const url = "http://192.168.1.6:3000/users"; // this url :- http://192.168.1.6:3000/users is working but this url :- http://10.0.2.2:3000/users
+  const getAPIData = async()=>{ 
     const response = await fetch(url);
     const jsonresponse:Get[] = await response.json();
     // console.log("data :- ",jsonresponse);
@@ -25,6 +30,7 @@ export default function App() {
   }
 
   useEffect(()=>{
+    // console.log("hello 1");
     getAPIData();
   },[]);
 
@@ -32,15 +38,16 @@ export default function App() {
     return (
       <View style={{padding:10, borderBottomColor:'white', borderBottomWidth:1, marginBottom:15}}>
         <Text style={{fontSize:20, backgroundColor:'gray', padding:10,marginBottom:10,textAlign:'center' }}>id :- {item.id}</Text>
-        <Text style={{fontSize:20 }}>Title :- {item.title}</Text>
-        <Text style={{fontSize:20,marginBottom:10 }}>Body :- {item.body}</Text>
+        <Text style={{fontSize:20 }}>Name :- {item.name}</Text>
+        <Text style={{fontSize:20 }}>Class :- {item.class}</Text>
+        <Text style={{fontSize:20,marginBottom:10 }}>Age :- {item.age}</Text>
       </View>
     )
   }
 
   return(
     <View style={{flex:1}}>
-      <Text style={{fontSize:25, padding:10, backgroundColor:'skyblue',marginBottom:8}}>User List From API :- </Text>
+      <Text style={{fontSize:25, padding:10, backgroundColor:'skyblue',marginBottom:8}}>Crated JSON API and Show User-List Data  :- </Text>
       
       {
         data?
